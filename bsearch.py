2d064fe1be6cd21f247d5f49b72c974f50a2c414
Artem <artger@ukr.net>
2019-12-10 15:14:35 +0200

    version 1

--- bsearch.py
+++ bsearch.py
@@ -1,6 +1,21 @@
+'''
+Binary search algorithm
+'''
 from typing import Iterable
 
 
-def bsearch(l: Iterable[int], v:int) -> int:
+def bsearch(lst: Iterable[int], value: int) -> int:
     """l always is sorted"""
-    return 42
+    lst = list(lst)
+    start, finish = 0, len(lst) - 1
+    if not lst or not value:
+        raise ValueError
+    while start <= finish:
+        mid = (start + finish) // 2
+        if value == lst[mid]:
+            return mid
+        if value > lst[mid]:
+            start = mid + 1
+        else:
+            finish = mid - 1
+    raise ValueError
