7a6c79ea87e215b00c847ab0c6ab4c0c3058ce10
d1makov <makovskyj.danylo@gmail.com>
2019-12-10 20:00:06 +0200

    final

--- test_bsearch.py
+++ test_bsearch.py
@@ -1,24 +1,48 @@
+"""
+test docstring
+"""
+
 import pytest
 import bsearch
 
 
 def test_empty():
+    """
+    test if list is empty
+    :return:
+    """
     with pytest.raises(ValueError):
-        bsearch.bsearch([], 0)
+        bsearch.bin_search([], 0)
 
 
 def test_not_found():
+    """
+    test if number equals 0
+    :return:
+    """
     with pytest.raises(ValueError):
-        bsearch.bsearch([1, 2, 3], 0)
+        bsearch.bin_search([1, 2, 3], 0)
 
 
 def test_even():
-    assert bsearch.bsearch([1, 2, 3, 4, 5, 6, 7, 8], 2) == 1
+    """
+    find 2 in the list
+    :return:
+    """
+    assert bsearch.bin_search([1, 2, 3, 4, 5, 6, 7, 8], 2) == 1
 
 
 def test_odd():
-    assert bsearch.bsearch([1, 2, 3, 4, 5, 6, 7], 2) == 1
+    """
+    find 2 in the list
+    :return:
+    """
+    assert bsearch.bin_search([1, 2, 3, 4, 5, 6, 7], 2) == 1
 
 
 def test_dup():
-    assert bsearch.bsearch([1, 2, 2, 2, 2, 2, 3, 3], 2) == 3
+    """
+    find 2 in the list
+    :return:
+    """
+    assert bsearch.bin_search([1, 2, 2, 2, 2, 2, 3, 3], 2) == 3
