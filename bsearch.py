30caa6bda63fb697c08be01855dcc81f33ae0184
ValentinaLV <tina.lysenok@gmail.com>
2019-12-10 12:12:54 +0200

    hw17_py_bsearch

--- bsearch.py
+++ bsearch.py
@@ -1,6 +1,29 @@
+"""
+Module binary search
+"""
 from typing import Iterable
 
 
-def bsearch(l: Iterable[int], v:int) -> int:
-    """l always is sorted"""
-    return 42
+def bsearch(lst: Iterable[int], value: int):
+    """
+    Binary search implementation
+    :param lst: Iterable[int]
+    :param value: int
+    :return: int
+    """
+
+    if not lst or value == 0:
+        raise ValueError
+
+    array = sorted(lst)
+
+    lower, high = 0, len(array) - 1
+    while lower <= high:
+        mid = (lower + high) // 2
+        if array[mid] < value:
+            lower = mid + 1
+        elif value < array[mid]:
+            high = mid - 1
+        else:
+            return mid
+    return None
