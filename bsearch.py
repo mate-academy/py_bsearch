a35c94acbb99a8b3d2a39bf66b3b93521fce1fe7
Dima <dima.sapsay@gmail.com>
2019-12-11 07:56:13 +0200

    first commit

--- bsearch.py
+++ bsearch.py
@@ -1,6 +1,25 @@
+"""
+Implement binary search algorithm
+(https://en.wikipedia.org/wiki/Binary_search_algorithm).
+"""
+
+
 from typing import Iterable
 
 
-def bsearch(l: Iterable[int], v:int) -> int:
-    """l always is sorted"""
-    return 42
+def bsearch(arr: Iterable[int], value: int) -> int:
+    """binary search algorithm"""
+    if not arr:
+        raise ValueError
+    arr = list(arr)
+    left = 0
+    right = len(list(arr)) - 1
+    while left < right - 1:
+        mid = (left + right) // 2
+        if arr[mid] == value:
+            return mid
+        if arr[mid] > value:
+            right = mid
+        else:
+            left = mid
+    raise ValueError
