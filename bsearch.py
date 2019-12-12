6c06f72d1c7b786537333fc6c6e5680192a1719a
Platoniuks <platon@alterpost.org>
2019-12-12 08:10:49 +0200

    resolved

--- bsearch.py
+++ bsearch.py
@@ -1,6 +1,22 @@
-from typing import Iterable
+"""Simple binary search"""
 
 
-def bsearch(l: Iterable[int], v:int) -> int:
-    """l always is sorted"""
-    return 42
+def bsearch(arr, value):
+    """
+    Simple binary search
+    :param arr:
+    :param value:
+    :return:
+    """
+    low = 0
+    high = len(arr) - 1
+    while low <= high and arr:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == value:
+            return mid
+        if guess > value:
+            high = mid - 1
+        else:
+            low = mid + 1
+    raise ValueError
